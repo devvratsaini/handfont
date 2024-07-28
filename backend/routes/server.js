eff06cc0ffa56379f77cc801e9a5f48863788597
pranav-suri <pranavsuri4@gmail.com>
2024-07-28 17:06:30 +0530

    added cors policy

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -1,14 +1,16 @@
 const { PORT } = require("../config");
+const cors = require("cors");
 const express = require("express");
-const routesRouter = require("./router")
+const routesRouter = require("./router");
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routesRouter);
 

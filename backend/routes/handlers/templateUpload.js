1de3530d2f2c7c129720f3a5c0fb47b9cb4502f4
pranav-suri <pranavsuri4@gmail.com>
2024-07-28 17:04:42 +0530

    Added types of templateUplaod

--- backend/routes/handlers/templateUpload.js
+++ backend/routes/handlers/templateUpload.js
@@ -9,35 +9,52 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 module.exports = async function templateUpload(req, res) {
-  upload.single("file")(req, res, async (err) => {
-    if (err) {
-      return res.status(400).json({ error: "File upload error" });
-    }
+    upload.single("file")(req, res, async (err) => {
+        if (err) {
+            return res.status(400).json({ error: "File upload error" });
+        }
 
-    const uploadedFile = req.file;
+        const uploadedFile = req.file;
 
-    if (!uploadedFile) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
+        if (!uploadedFile) {
+            return res.status(400).json({ error: "No file uploaded" });
+        }
 
-    try {
-      const splitImages = await gridSplitter(uploadedFile.buffer, true);
-      const svgIconResponse = {
-        fontName: "",
-        characters: [],
-      };
+        try {
+            const splitImages = await gridSplitter(uploadedFile.buffer, true);
+            /**
+             * @typedef {Object} Character
+             * @property {string} char - The character.
+             * @property {string} svgString - The SVG string.
+             */
 
-      for ({imageData, char} of splitImages) {
-        const svgString = await imageToSvg(imageData);
-        svgIconResponse.characters.push({
-          char,
-          svgString,
-        });
-      }
-      
-      res.json(svgIconResponse);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to process image" });
-    }
-  });
+            /**
+             * @typedef {Object} SvgIconResponse
+             * @property {string} fontName - The name of the font.
+             * @property {Character[]} characters - The array of character objects.
+             */
+
+            /**
+             * @type {SvgIconResponse}
+             */
+            const svgIconResponse = {
+                fontName: "",
+                characters: [],
+            };
+            if (!splitImages) {
+                return res.status(500).json({ error: "Failed to split image" });
+            }
+            for (const { imageData, char } of splitImages) {
+                const svgString = await imageToSvg(imageData);
+                svgIconResponse.characters.push({
+                    char,
+                    svgString,
+                });
+            }
+
+            res.json(svgIconResponse);
+        } catch (error) {
+            res.status(500).json({ error: "Failed to process image" });
+        }
+    });
 };

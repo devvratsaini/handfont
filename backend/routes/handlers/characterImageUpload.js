e572439f747ae1cfd415504c2e72c21ed557a5a7
devvratsaini <devvratsaini4@gmail.com>
2024-07-28 16:31:16 +0530

    implemented characterImageUpload.js route

--- backend/routes/handlers/characterImageUpload.js
+++ backend/routes/handlers/characterImageUpload.js
@@ -1,4 +1,33 @@
-// This function will convert the image into the svg format, and return the an
-// object in the form { char: 'A', svg: '<svg>...</svg>' }
+// previously named 'characterImageUpload.js',
+// This function will convert a single character image into its svg format,
+// and return it in the form { char: 'A', svg: '<svg>...</svg>' }
 
-module.exports = async function characterImageUpload(req, res) {};
+const fs = require("fs");
+
+const imageToSvg = require("../../controllers/imageToSvg");
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+module.exports = async function characterImageUpload(req, res) {
+  upload.single("image")(req, res, async (err) => {
+    if (err) {
+      return res.status(400).json({ error: "File upload error" });
+    }
+
+    const requestedFile = req.file;
+
+    if (!requestedFile) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+
+    try {
+      const svgString = await imageToSvg(requestedFile.buffer);
+
+      res.status(200).json({svgString});
+    } catch (error) {
+      res.status(500).json({ error: "Failed to process image" });
+    }
+  });
+};
